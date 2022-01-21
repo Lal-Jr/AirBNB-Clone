ffda5b9d927495f1c44bfa0ce4b26602a22a17b1
Harish Lal <69763020+Lal-Jr@users.noreply.github.com>
2022-01-21 23:56:26 +0530

    header layout added

---
+++ components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+	return (
+		<header className="w-full bg-indigo-50 h-16 flex justify-center items-center">
+			Something i want to show
+		</header>
+	);
+};
+
+export default Header;
