03c9959fffdab5a0ae221094bca85999d92bbbeb
Harish Lal <69763020+Lal-Jr@users.noreply.github.com>
2022-01-24 10:30:39 +0530

    padding change

--- components/Header.jsx
+++ components/Header.jsx
@@ -8,7 +8,7 @@ import {
 
 const Header = () => {
 	return (
-		<header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:p-10">
+		<header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md py-2.5 px-5 md:py-5 md:px-10">
 			<div className="relative flex items-center h-10 cursor-pointer my-auto">
 				<Image
 					src="https://links.papareact.com/qd3"
