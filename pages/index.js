7cd498fdd88df184eaaa0833cf33d05d5b11743e
Harish Lal <69763020+Lal-Jr@users.noreply.github.com>
2022-01-24 23:29:56 +0530

    fetching data and rendering it check

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import { Header, Banner } from "../components";
 
-export default function Home() {
+export default function Home({ exploreData }) {
 	return (
 		<div className="min-h-screen">
 			<Head>
@@ -10,6 +10,28 @@ export default function Home() {
 			</Head>
 			<Header />
 			<Banner />
+			<main className="max-w-7xl mx-auto px-8 sm:px-16">
+				<section className="pt-6">
+					<h2 className="text-4xl font-semibold pb-5">
+						Explore Nearby
+					</h2>
+					{exploreData?.map((item) => (
+						<h1>{item.location}</h1>
+					))}
+				</section>
+			</main>
 		</div>
 	);
+}
+
+export async function getStaticProps() {
+	const exploreData = await fetch("https://links.papareact.com/pyp").then(
+		(res) => res.json()
+	);
+
+	return {
+		props: {
+			exploreData,
+		},
+	};
 }
